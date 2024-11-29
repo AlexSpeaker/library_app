7f97b39aa5943f1eacb9ca799903ebc948950e86
AlexSokolov <speaker04@mail.ru>
2024-11-29 22:36:26 +0300

    add docstring

--- library_app/core/orm/classes/model.py
+++ library_app/core/orm/classes/model.py
@@ -4,8 +4,16 @@ from typing import Any, Dict
 
 @dataclass
 class BaseModel:
-
+    """Базовый класс модели базы данных"""
     def looks_like_strictly(self, **kwargs: str | int | bool) -> bool:
+        """
+        Строгое соответствие.
+        Вернёт True, если поле модели строго эквивалентно переданному значению.
+        Если передаётся несколько значений, то True будет только если все сравнения вернут True.
+
+        :param kwargs: Словарь для сравнения с моделью.
+        :return: True или False.
+        """
         return all(
             (
                 getattr(self, key).lower() == value.lower()
@@ -16,6 +24,15 @@ class BaseModel:
         )
 
     def looks_like_softly(self, **kwargs: str | int | bool) -> bool:
+        """
+        Мягкое соответствие.
+        Вернёт True, если поле модели начинается с переданного значения.
+        Работает только для str, для других типов сравнение строгое.
+        Если передаётся несколько значений, то True будет только если все сравнения вернут True.
+
+        :param kwargs: Словарь для сравнения с моделью.
+        :return: True или False.
+        """
         return all(
             (
                 getattr(self, key).lower().startswith(value.lower())
@@ -26,6 +43,12 @@ class BaseModel:
         )
 
     def to_dict(self) -> Dict[str, Any]:
+        """
+        Конвертирует модель в словарь.
+
+        :return: Словарь.
+        """
+
         # Почему-то у меня тут pycharm подсвечивает self, хотя у mypy вопросов нет.
         return {
             key: value
--- library_app/core/orm/classes/orm.py
+++ library_app/core/orm/classes/orm.py
@@ -7,13 +7,23 @@ from settings.settings_class import Settings
 
 
 class BaseORM:
+    """Базовый класс ORM"""
     def __init__(self, settings: Settings):
+        """
+        Инициализация класса.
+        :param settings: Подключаемые настройки.
+        """
         self.__settings: Settings = settings
         db_dir: Path = settings.db_settings.db_base_path
         db_name: str = settings.db_settings.name
         self.__db_path: Path = db_dir / db_name
 
     def _get_database(self) -> Dict[str, List[Dict[str, Any]]]:
+        """
+        Функция получит json из файла и вернёт его в виде словаря.
+
+        :return: Словарь.
+        """
         with open(self.__db_path, "r", encoding="utf-8") as file:
             db = json.load(file)
         if not isinstance(db, dict):
@@ -21,20 +31,44 @@ class BaseORM:
         return db
 
     def _save_db(self, db: Dict[str, List[Dict[str, Any]]]) -> None:
+        """
+        Функция принимает словарь и записывает его в файл как json объект.
+
+        :param db:
+        :return:
+        """
         with open(self.__db_path, "w", encoding="utf-8") as file:
             json.dump(
                 db, file, ensure_ascii=False, indent=4
-            )  # Почему-то у меня тут pycharm ругается, хотя у mypy вопросов нет.
+            )  # Почему-то у меня тут pycharm подсвечивает file, хотя у mypy вопросов нет.
 
 
 class ORM(BaseORM):
+    """Класс ORM"""
     def __init__(self, settings: Settings):
+        """
+        Инициализация класса.
+        :param settings: Подключаемые настройки.
+        """
         super().__init__(settings)
 
     def select(self, model: Any) -> QuerySet:
+        """
+        Функция принимает модель, передает его и базу данных классу QuerySet.
+        Возвращает Экземпляр класса QuerySet.
+
+        :param model: Модель.
+        :return: Экземпляр класса QuerySet.
+        """
         return QuerySet(model, self._get_database())
 
     def add(self, model: Any) -> Any:
+        """
+        Функция принимает модель и записывает её в базу данных.
+
+        :param model: Модель.
+        :return: Вернёт модель с id.
+        """
         db = self._get_database()
         table_name = model.__tablename__
         db.setdefault(table_name, [])
@@ -45,6 +79,12 @@ class ORM(BaseORM):
         return model
 
     def delete(self, model: Any) -> None:
+        """
+        Функция принимает модель, находит её в бд по id и удаляет её.
+
+        :param model: Модель.
+        :return: None.
+        """
         db = self._get_database()
         table_name = model.__tablename__
         table = db.setdefault(table_name, [])
@@ -52,6 +92,12 @@ class ORM(BaseORM):
         self._save_db(db)
 
     def update(self, model: Any) -> None:
+        """
+        Функция принимает модель, находит её в бд по id и обновляет данные.
+
+        :param model: Модель.
+        :return: None.
+        """
         db = self._get_database()
         table_name = model.__tablename__
         table = db.setdefault(table_name, [])
